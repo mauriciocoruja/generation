3794c2458470f2cf5a54a8d691e608e10603671c
Mauricio Barbosa <mbjr@outlook.com>
2019-05-20 11:06:10 -0300

    scrip otimizado

--- Exercicios/Aula-6-JavaScript/js/script.js
+++ Exercicios/Aula-6-JavaScript/js/script.js
@@ -1,7 +1,12 @@
-var diaDaSemana = "Domingo";
+var diaDaSemana = "Sexta-Feira";
 
 if (diaDaSemana == "Domingo") {
 	console.log("Dia do churrasco");
+}else if (diaDaSemana == "Quarta-feira") {
+	console.log("Dia de feijoada");	
+}else if (diaDaSemana == "Sexta-Feira") {
+	console.log("Decreto");	
 }else{
 	console.log("Hoje não é dia de churrasco");
 }
+console.log("Programa finalizado");
