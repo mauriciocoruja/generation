12f8feb167c83b277e1f8c47022c5da178a64d43
unset <unset>
2019-05-22 13:46:13 -0300

    Seletores | Exercicio 2 - Resolvido

--- Exercicios/DOM/assets/js/main.js
+++ Exercicios/DOM/assets/js/main.js
@@ -1,3 +1,8 @@
 // Exercicio 1
 document.getElementById('cabecalho');
 	cabecalho.style.display = "none";
+
+// Exercicio 2
+document.querySelector('#lechuza');
+	lechuza.style.filter = "grayscale(100%)";
+
