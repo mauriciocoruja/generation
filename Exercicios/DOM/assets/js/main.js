39ae176871dcc617a81109609984368e4a015a16
unset <unset>
2019-05-22 14:42:58 -0300

    Exercicio 2 - Visualizar o atributo href de um determinado item

--- Exercicios/DOM/assets/js/main.js
+++ Exercicios/DOM/assets/js/main.js
@@ -6,7 +6,7 @@ document.querySelector('#lechuza').style.filter = "grayscale(100%)";
 
 // Exercicio 3
 
-let entradas = document.querySelectorAll("input");
+let entradas = document.querySelectorAll("input, textarea");
 for (var i = 0; i < entradas.length; i++) {
 	entradas[i].style.backgroundColor = "red";
 }
@@ -17,5 +17,10 @@ for (var i = 0; i < entradas.length; i++) {
 // Exercicio 1
 document.getElementById('copyright');
 console.log(copyright.attributes);
+
+// Exercicio 2
+let tt = document.querySelector(".fa-twitter");
+tt.getAttribute('href');
+console.log(tt);
 
 //
