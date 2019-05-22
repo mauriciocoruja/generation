a26e96ff34de6415d3a1ca7db353997426bbd573
unset <unset>
2019-05-22 14:04:54 -0300

    Seletores | Exercicio 3 resolvido

--- Exercicios/DOM/assets/js/main.js
+++ Exercicios/DOM/assets/js/main.js
@@ -4,3 +4,11 @@ document.getElementById('cabecalho').style.display = "none";
 // Exercicio 2
 document.querySelector('#lechuza').style.filter = "grayscale(100%)";
 
+// Exercicio 3
+
+let entradas = document.querySelectorAll("input");
+for (var i = 0; i < entradas.length; i++) {
+	entradas[i].style.backgroundColor = "red";
+}
+
+// 
