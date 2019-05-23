deeb0f4958f44112290254b31d9251a195bf9dec
unknown <Digital@DH-A2-08.dhbrasil.digitalhouse.com>
2019-05-23 14:18:35 -0300

    Exercicios de fixação sobre Eventos

--- Exercicios/Eventos/eventos.js
+++ Exercicios/Eventos/eventos.js
@@ -1,4 +1,27 @@
 let btnClique = document.getElementById("clique");
+// btnClique.onmouseover = function(){
+//     alert("Tô ligado que você clicou")
+// }
+
+// let inputText = document.getElementById("texto");
+// btnClique.onclick = function(){
+//     inputText.style.opacity = "0"
+// }
+
+let inputText = document.getElementById("texto");
 btnClique.onclick = function(){
-    alert("Tô ligado que você clicou")
+    if(inputText.style.opacity == "0"){
+        inputText.style.opacity = "100";
+        inputText.style.backgroundColor = "blue";
+    }else{
+        inputText.style.opacity = "0";
+    }
 }
+
+inputText.onkeydown = function (event) {
+    let keyCode = event.keyCode;
+    if (keyCode == 13) {
+        alert("Apertou o enter?");
+    }
+}
+
