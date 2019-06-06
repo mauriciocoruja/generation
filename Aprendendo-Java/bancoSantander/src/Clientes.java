83028099a5eb4a3f0c8860b56fc0556428b47298
mauriciocoruja <mbjr@outlook.com.br>
2019-06-06 10:48:06 -0300

    Reorganização de pastas e arquivos. Iniciado Exercicio BancoSantander

---
+++ Aprendendo-Java/bancoSantander/src/Clientes.java
@@ -0,0 +1,47 @@
+public class Clientes {
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Clientes(String nome, String sobrenome, String rg, String cpf) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+        this.cpf = cpf;
+    }
+
+    
+}
