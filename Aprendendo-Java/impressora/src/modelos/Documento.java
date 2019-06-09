d260932ed92f8c83822b755cfc8007e09516b732
mauriciocoruja <mbjr@outlook.com.br>
2019-06-09 13:26:24 -0300

    definidos metodos de impressão

--- Aprendendo-Java/impressora/src/modelos/Documento.java
+++ Aprendendo-Java/impressora/src/modelos/Documento.java
@@ -1,4 +1,11 @@
 package modelos;
 
 public class Documento implements Imprimivel {
+
+    private String nome;
+    private String tipo;
+
+    public void imprimir(){
+        System.out.println("Sou um documento do word\n"+nome+"\n"+tipo);
+    }
 }
--- Aprendendo-Java/impressora/src/modelos/Foto.java
+++ Aprendendo-Java/impressora/src/modelos/Foto.java
@@ -1,4 +1,10 @@
 package modelos;
 
 public class Foto implements Imprimivel {
+    private String nome;
+    private String tipo;
+
+    public void imprimir(){
+        System.out.println("Sou uma selfie\n"+nome+"\n"+tipo);
+    }
 }
