e9b7942325a2e7668222495ec480aa8514e751fc
mauriciocoruja <mbjr@outlook.com.br>
2019-06-09 16:59:37 -0300

    Exercicio 4 - Construção base

--- Aprendendo-Java/exercicios-equals/src/exercicioQuatro/ExercicioQuatro.java
+++ Aprendendo-Java/exercicios-equals/src/exercicioQuatro/ExercicioQuatro.java
@@ -1,7 +1,12 @@
 package exercicioQuatro;
 
+//import java.util.ArrayList;
+
 public class ExercicioQuatro {
     public static void main(String[] args) {
+//        ArrayList<String> funcionarios = new ArrayList<String>();
+
+
 
     }
 }
--- Aprendendo-Java/exercicios-equals/src/exercicioQuatro/modelos/Funcionario.java
+++ Aprendendo-Java/exercicios-equals/src/exercicioQuatro/modelos/Funcionario.java
@@ -1,4 +1,26 @@
 package exercicioQuatro.modelos;
 
+import java.util.Objects;
+
 public class Funcionario {
+    private String nome;
+    private Integer numeroDeRegistro;
+
+    public Funcionario(String nome, Integer numeroDeRegistro) {
+        this.nome = nome;
+        this.numeroDeRegistro = numeroDeRegistro;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return numeroDeRegistro.equals(that.numeroDeRegistro);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numeroDeRegistro);
+    }
 }
