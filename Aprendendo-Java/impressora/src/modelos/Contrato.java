992ee34f79c50876f7a4cab1b7fe274fb0e00f33
mauriciocoruja <mbjr@outlook.com.br>
2019-06-09 13:13:34 -0300

    Criado metodo imprimir em contrato

--- Aprendendo-Java/impressora/src/modelos/Contrato.java
+++ Aprendendo-Java/impressora/src/modelos/Contrato.java
@@ -2,4 +2,12 @@ package modelos;
 
 public class Contrato implements Imprimivel{
 
+    private String nome;
+    private String tipo;
+
+    public void imprimir(){
+        System.out.println("Sou um contrato muito leal\n"+nome+"\n"+tipo);
+    }
+
+
 }
