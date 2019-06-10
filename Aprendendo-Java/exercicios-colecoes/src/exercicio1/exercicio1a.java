a0530a7b420a87936e301a7249bc734208fdbba8
mauriciocoruja <mbjr@outlook.com.br>
2019-06-10 16:16:26 -0300

    Exercicio 1b e 1b

---
+++ Aprendendo-Java/exercicios-colecoes/src/exercicio1/exercicio1a.java
@@ -0,0 +1,29 @@
+package exercicio1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class exercicio1a {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> dicionario = new HashMap<>();
+
+        dicionario.put(0,"Ovos");
+        dicionario.put(1,"Água");
+        dicionario.put(2,"Escopeta");
+        dicionario.put(3,"Cavalo");
+        dicionario.put(4,"Dentista");
+        dicionario.put(5,"Fogo");
+
+        System.out.println(dicionario);
+
+        for (Integer palavra : dicionario.keySet()){
+            System.out.println(dicionario.get(palavra));
+        }
+
+        //System.out.println(dicionario.get(1));
+
+
+    }
+}
---
+++ Aprendendo-Java/exercicios-colecoes/src/exercicio1/exercicio1b.java
@@ -0,0 +1,44 @@
+package exercicio1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class exercicio1b {
+    public static void main(String[] args) {
+        Map<String, List<String>> apelidos = new HashMap<>();
+
+        List<String> joao = new ArrayList<>();
+        joao.add("Juan");
+        joao.add("Fissure");
+        joao.add("Maromba");
+        apelidos.put("João", joao);
+
+        List<String> miguel = new ArrayList<>();
+        miguel.add("Night Watch");
+        miguel.add("Bruce Wayne");
+        miguel.add("Tampinha");
+        apelidos.put("Miguel", miguel);
+
+        List<String> maria = new ArrayList<>();
+        maria.add("Wonder");
+        maria.add("Mary");
+        maria.add("Marilene");
+        apelidos.put("Maria", maria);
+
+        List<String> lucas = new ArrayList<>();
+        lucas.add("Lukinha");
+        lucas.add("Jorge");
+        lucas.add("George");
+        apelidos.put("Lucas", lucas);
+
+        System.out.println(apelidos+"\n");
+
+        for(String apelido : apelidos.keySet()){
+            System.out.println("Nome: " + apelido + " " +apelidos.get(apelido));
+        }
+
+    }
+
+}
