4d45166c8a3b2348f1fcaa2e3d56a51a9812240f
mauriciocoruja <mbjr@outlook.com.br>
2019-06-11 11:29:45 -0300

    Primeira parte do exercicio

---
+++ Aprendendo-Java/exception/src/exercicios/exercicio1.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+
+        List<String> animais = new ArrayList<>();
+
+        animais.add("Pato");
+        animais.add("Cachorro");
+        animais.add("Gato");
+
+        System.out.println(animais.get(3));
+
+    }
+}
