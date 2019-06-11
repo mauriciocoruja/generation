23561603eeb8c469dc68d56737826f1ed20bec47
mauriciocoruja <mbjr@outlook.com.br>
2019-06-11 14:32:04 -0300

    Exercicio 1

---
+++ Aprendendo-Java/revisao-java/src/exercicio1/Exercicio1.java
@@ -0,0 +1,17 @@
+package exercicio1;
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        Scanner numero = new Scanner(System.in);
+        System.out.printf("Digite um numero: ");
+
+        int a;
+
+        a = numero.nextInt();
+        
+        System.out.println("Antecessor: "+(a-1)+"\nNumero: "+a+"\nSucessor: "+(a+1));
+
+    }
+}
