a3c0f200c24abdc2d5c29315dcfcc1f1e29367d8
mauriciocoruja <mbjr@outlook.com.br>
2019-06-11 17:20:17 -0300

    Exercicio 2

---
+++ Aprendendo-Java/revisao-java/src/exercicios/exercicio2/Exercicio2.java
@@ -0,0 +1,26 @@
+package exercicios.exercicio2;
+import exercicios.exercicio2.modelo.Pessoa;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Fulano",23);
+        Pessoa p2 = new Pessoa("Ciclano",21);
+        Pessoa p3 = new Pessoa("Beltrano",28);
+
+//        if (p1.getIdade() > p2.getIdade() && p1.getIdade() > p3.getIdade()){
+//            System.out.println(p1.getNome()+" é mais o velho");
+//        } else if (p1.getIdade() < p2.getIdade() && p1.getIdade() < p3.getIdade()){
+//            System.out.println(p1.getNome()+" é mais o novo");
+//        }
+//        if (p2.getIdade() < p3.getIdade() && p2.getIdade() < p3.getIdade()){
+//            System.out.println(p2.getNome()+" é mais o novo");
+//        } else if (p3.getIdade() < p1.getIdade() && p3.getIdade() < p2.getIdade()){
+//            System.out.println(p1.getNome()+" é mais o novo");
+//        }
+
+
+
+
+
+    }
+}
---
+++ Aprendendo-Java/revisao-java/src/exercicios/exercicio2/modelo/Pessoa.java
@@ -0,0 +1,32 @@
+package exercicios.exercicio2.modelo;
+
+public class Pessoa {
+    private String nome;
+    private Integer idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Pessoa(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void obterMaisVelho(Pessoa nome){
+        nome.getIdade();
+    }
+
+}
