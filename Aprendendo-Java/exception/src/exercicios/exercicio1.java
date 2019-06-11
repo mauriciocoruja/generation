1f7c3f527d4f0c9c7b1258e224e9001f0aabaf74
mauriciocoruja <mbjr@outlook.com.br>
2019-06-11 11:42:24 -0300

    primeira parte exercicio 2

--- Aprendendo-Java/exception/src/exercicios/exercicio1.java
+++ Aprendendo-Java/exception/src/exercicios/exercicio1.java
@@ -3,7 +3,7 @@ package exercicios;
 import java.util.ArrayList;
 import java.util.List;
 
-public class exercicio1 {
+public class Exercicio1 {
     public static void main(String[] args) {
 
         try{
