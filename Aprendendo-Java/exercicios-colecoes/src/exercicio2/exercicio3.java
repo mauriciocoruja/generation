9ee003ae80cee7b7a40f783662734907acc20b27
mauriciocoruja <mbjr@outlook.com.br>
2019-06-15 10:16:40 -0300

    exercicio 2. Melhoria na organização

--- Aprendendo-Java/exercicios-colecoes/src/exercicio2/exercicio3.java
+++ Aprendendo-Java/exercicios-colecoes/src/exercicio2/exercicio3.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class exercicio3 {
     public static void main(String[] args) {
+
+    /*Definir e inicializar uma nova lista vazia.
+    Adicionar os seguintes elementos à lista vazia: 1,5,5,6,7,8,8,8.*/
+
         List<Integer> lista = new ArrayList<>();
 
         lista.add(1);
@@ -17,8 +21,33 @@ public class exercicio3 {
 
         System.out.println(lista);
 
+    /*Imprimir o resultado na tela.*/
+
         for (Integer listItem : lista){
             System.out.println(listItem);
         }
+
+    /*Definir e inicializar um novo conjunto vazio.
+    Adicionar os seguintes elementos ao conjunto vazio: 1,5,5,6,7,8,8,8.*/
+
+        Set<Integer> conjunto = new HashSet<>();
+        conjunto.add(1);
+        conjunto.add(5);
+        conjunto.add(5);
+        conjunto.add(6);
+        conjunto.add(7);
+        conjunto.add(8);
+        conjunto.add(8);
+        conjunto.add(8);
+        System.out.println(conjunto);
+
+        /*Imprimir o resultado na tela. */
+
+        for (Integer itemDoConjunto : conjunto){
+            System.out.println(itemDoConjunto);
+        }
+
+        /*Qual é a diferença entre este exercício e o anterior?*/
+        //A diferença é que, usando o Set ele não aceita dados repetidos
     }
 }
