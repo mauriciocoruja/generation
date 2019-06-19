0456db1ddf37c366054e7557d9b11b58ebacfe9d
mauriciocoruja <mbjr@outlook.com.br>
2019-06-19 11:35:30 -0300

    Inserindo e visualizando dados no banco de dados via Java

--- Aprendendo-Java/restaurante/src/Insert.java
+++ Aprendendo-Java/restaurante/src/Insert.java
@@ -1,15 +1,29 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import connection.ConnectionFactory;
+
+import java.sql.*;
 
 public class Insert {
     public static void main(String[] args) {
-
+        Connection con = null;
+        PreparedStatement stmt = null;
         try {
-            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/restaurante?serverTimezone=GMT-3","root","");
+            con = new ConnectionFactory().getConnection();
+            String sql = "INSERT INTO cliente (id_cliente, nome) VALUES(?,?)";
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1,3);
+            stmt.setString(2,"Ana Maria");
+
+            stmt.execute();
+
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("Conectado");
     }
 }
