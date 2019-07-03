68f13a2f6646bf6d316085433be49addd499be71
mauriciocoruja <mbjr@outlook.com.br>
2019-07-03 17:19:12 -0300

    Dados SQL

---
+++ Aprendendo-Java/Spring/gfood/src/main/resources/mysql/gfood_cliente.sql
@@ -0,0 +1,4 @@
+insert into gfood.cliente (id, nome, cpf, endereco, datanascimento) values (1, 'Carlota', '123.443.221-12', 'Rua Pacheco', '1982-12-24');
+insert into gfood.cliente (id, nome, cpf, endereco, datanascimento) values (2, 'Pedro', '123.123.123-2', 'rua aurorinha,32', '2000-01-01');
+insert into gfood.cliente (id, nome, cpf, endereco, datanascimento) values (3, 'Lucas Siqueira', '123.987.987-87', 'rua Moreira, 652', '1989-01-02');
+insert into gfood.cliente (id, nome, cpf, endereco, datanascimento) values (4, 'Lucas Almeira', '123.123.987-87', 'rua Porto, 652', '1989-01-02');
---
+++ Aprendendo-Java/Spring/gfood/src/main/resources/mysql/gfood_produto.sql
@@ -0,0 +1,3 @@
+insert into gfood.produto (id, nome, descricao, preco) values (1, 'Coxinha', 'Melhor coxinha de São Paulo', 3);
+insert into gfood.produto (id, nome, descricao, preco) values (2, 'Risole', 'Risole mais top que você vai comer em toda a sua vida. Confia na call', 5);
+insert into gfood.produto (id, nome, descricao, preco) values (3, 'Kibe', 'Topzera da Bahia', 7.2);
