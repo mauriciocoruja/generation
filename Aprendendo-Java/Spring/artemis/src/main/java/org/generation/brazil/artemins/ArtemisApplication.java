d537285c1719abea73e9084ce6eaa34287f874af
mauriciocoruja <mbjr@outlook.com.br>
2019-07-12 14:12:16 -0300

    Ajustado o dialeto do MySQL; alterado o nome da classe

--- Aprendendo-Java/Spring/artemis/src/main/java/org/generation/brazil/artemins/ArtemisApplication.java
+++ Aprendendo-Java/Spring/artemis/src/main/java/org/generation/brazil/artemins/ArtemisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArteminsApplication {
+public class ArtemisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArteminsApplication.class, args);
+        SpringApplication.run(ArtemisApplication.class, args);
     }
 
 }
