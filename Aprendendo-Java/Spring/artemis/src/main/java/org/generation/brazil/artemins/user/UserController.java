8190683bdb3eb4e5c1bb4bc1a7f8cc08bc885201
mauriciocoruja <mbjr@outlook.com.br>
2019-07-12 14:05:33 -0300

    Criado o metodo Read e corrigido o metodo Create(alterada o endpoint; substituido o @RequestParam, para @RequestBody, pois o objeto vai no corpo da requisição)

--- Aprendendo-Java/Spring/artemis/src/main/java/org/generation/brazil/artemins/user/UserController.java
+++ Aprendendo-Java/Spring/artemis/src/main/java/org/generation/brazil/artemins/user/UserController.java
@@ -1,13 +1,18 @@
 package org.generation.brazil.artemins.user;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final UserRepository repository;
@@ -17,9 +22,16 @@ public class UserController {
     }
 
     //C
-    @PostMapping
-    public User create(@RequestParam User user){
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/user")
+    public User create(@RequestBody User user){
         return repository.save(user);
+    }
+
+    //R
+    @GetMapping("/users")
+    public List<User> findAll(){
+        return repository.findAll();
     }
 
 
